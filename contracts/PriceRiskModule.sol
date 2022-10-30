42ae4d52d467b23f2e7a36ab5533c062a7e6c771
GnP <g.parrondo@gmail.com>
2022-10-30 12:28:14 -0300

    Add storage gap

--- contracts/PriceRiskModule.sol
+++ contracts/PriceRiskModule.sol
@@ -390,4 +390,11 @@ contract PriceRiskModule is RiskModule, IPriceRiskModule {
   function minDuration() external view override returns (uint40) {
     return _state.minDuration;
   }
+
+  /**
+   * @dev This empty reserved space is put in place to allow future versions to add new
+   * variables without shifting down storage in the inheritance chain.
+   * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+   */
+  uint256[47] private __gap;
 }
