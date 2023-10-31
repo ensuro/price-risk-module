acea84d3eca4483b2b570d7eb98fa234106888a9
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2023-10-31 07:36:44 -0300

    Change AAVERepayPayoutAutomation to use Gelato
    The contract now inherits from PayoutAutomationGelatoBase, thus it pays
    the gelato fee when triggered.

--- contracts/payout-automations/AAVERepayPayoutAutomation.sol
+++ contracts/payout-automations/AAVERepayPayoutAutomation.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import {PayoutAutomationBase} from "./PayoutAutomationBase.sol";
+import {PayoutAutomationBaseGelato} from "./PayoutAutomationBaseGelato.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {IPolicyPool} from "@ensuro/core/contracts/interfaces/IPolicyPool.sol";
+import {IWETH9} from "../dependencies/uniswap-v3/IWETH9.sol";
 import {IPool} from "../dependencies/aave-v3/IPool.sol";
+import {IPriceOracle} from "../interfaces/IPriceOracle.sol";
 import {DataTypes} from "../dependencies/aave-v3/DataTypes.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-contract AAVERepayPayoutAutomation is PayoutAutomationBase {
+contract AAVERepayPayoutAutomation is PayoutAutomationBaseGelato {
   using SafeERC20 for IERC20Metadata;
 
   /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
@@ -17,7 +20,12 @@ contract AAVERepayPayoutAutomation is PayoutAutomationBase {
 
   /// @custom:oz-upgrades-unsafe-allow constructor
   // solhint-disable-next-line no-empty-blocks
-  constructor(IPolicyPool policyPool_, IPool aave_) PayoutAutomationBase(policyPool_) {
+  constructor(
+    IPolicyPool policyPool_,
+    address automate_,
+    IWETH9 weth_,
+    IPool aave_
+  ) PayoutAutomationBaseGelato(policyPool_, automate_, weth_) {
     require(
       address(aave_) != address(0),
       "AAVERepayPayoutAutomation: you must specify AAVE's Pool address"
@@ -32,9 +40,12 @@ contract AAVERepayPayoutAutomation is PayoutAutomationBase {
   function initialize(
     string memory name_,
     string memory symbol_,
-    address admin
+    address admin,
+    IPriceOracle oracle_,
+    ISwapRouter swapRouter_,
+    uint24 feeTier_
   ) public virtual initializer {
-    __PayoutAutomationBase_init(name_, symbol_, admin);
+    __PayoutAutomationBaseGelato_init(name_, symbol_, admin, oracle_, swapRouter_, feeTier_);
     // Infinite approval to AAVE to avoid approving every time
     _policyPool.currency().approve(address(_aave), type(uint256).max);
   }
--- test/test-aave-payout-automations.js
+++ test/test-aave-payout-automations.js
@@ -3,8 +3,15 @@ const hre = require("hardhat");
 const { ethers } = hre;
 const { AddressZero, MaxUint256 } = ethers.constants;
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const { anyUint } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { _W, _E, amountFunction, grantComponentRole, makePolicyId } = require("@ensuro/core/js/utils");
-const { deployPool, deployPremiumsAccount, addRiskModule, addEToken } = require("@ensuro/core/js/test-utils");
+const {
+  deployPool,
+  deployPremiumsAccount,
+  addRiskModule,
+  addEToken,
+  initForkCurrency,
+} = require("@ensuro/core/js/test-utils");
 const { fork } = require("./utils");
 
 const HOUR = 3600;
@@ -12,80 +19,66 @@ const HOUR = 3600;
 hre.upgrades.silenceWarnings();
 
 describe("Test AAVE payout automation contracts", function () {
-  let cust, cust2, lp, owner;
   let _A;
 
   const ADDRESSES = {
-    usdc: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+    USDC: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
     aaveV3: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
-    usrUSDC: "0x4d97dcd97ec945f40cf65f87097ace5ea0476045", // Random account with lot of USDC
+    USDCWhale: "0x4d97dcd97ec945f40cf65f87097ace5ea0476045", // Random account with lot of USDC
 
     // From USDC reserve data
     aUSDC: "0x625E7708f30cA75bfd92586e17077590C60eb4cD",
     aUSDCDebtStable: "0x307ffe186F84a3bc2613D1eA417A5737D69A7007",
     aUSDCDebtVariable: "0xFCCf3cAbbe80101232d343252614b6A3eE81C989",
 
-    wmatic: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
-    weth: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
-    ensuroTreasury: "0x913B9dff6D780cF4cda0b0321654D7261d5593d0", // Random address
-    etk: "0xCFfDcC8e99Aa22961704b9C7b67Ed08A66EA45Da",
-    variableDebtmUSDC: "0x248960A9d75EdFa3de94F7193eae3161Eb349a12",
-    oracle: "0x0229f777b0fab107f9591a41d5f02e4e98db6f2d", // AAVE PriceOracle
-    sushi: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506", // Sushiswap router
-    assetMgr: "0x09d9Dd252659a497F3525F257e204E7192beF132",
-    usrWMATIC: "0x55FF76BFFC3Cdd9D5FdbBC2ece4528ECcE45047e", // Random account with log of WMATIC
+    WMATIC: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    MATICWhale: "0x55FF76BFFC3Cdd9D5FdbBC2ece4528ECcE45047e", // Random account with lot of WMATIC
+
+    AUTOMATE: "0x527a819db1eb0e34426297b03bae11F2f8B3A19E",
+    SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
   };
 
   beforeEach(async () => {
-    [owner, lp, cust, cust2] = await ethers.getSigners();
-
     const decimals = 6;
     _A = amountFunction(decimals);
   });
 
   it("Should fail if constructed with null address ", async () => {
     const { pool, AAVERepayPayoutAutomation } = await helpers.loadFixture(deployPoolFixture);
-    await expect(AAVERepayPayoutAutomation.deploy(AddressZero, AddressZero)).to.be.revertedWith(
-      "PayoutAutomationBase: policyPool_ cannot be the zero address"
-    );
-    await expect(AAVERepayPayoutAutomation.deploy(pool.address, AddressZero)).to.be.revertedWith(
-      "AAVERepayPayoutAutomation: you must specify AAVE's Pool address"
-    );
-    await expect(AAVERepayPayoutAutomation.deploy(pool.address, ADDRESSES.aaveV3)).not.to.be.reverted;
+    await expect(
+      AAVERepayPayoutAutomation.deploy(AddressZero, ADDRESSES.AUTOMATE, ADDRESSES.WMATIC, AddressZero)
+    ).to.be.revertedWith("PayoutAutomationBase: policyPool_ cannot be the zero address");
+    await expect(
+      AAVERepayPayoutAutomation.deploy(pool.address, ADDRESSES.AUTOMATE, ADDRESSES.WMATIC, AddressZero)
+    ).to.be.revertedWith("AAVERepayPayoutAutomation: you must specify AAVE's Pool address");
+    await expect(AAVERepayPayoutAutomation.deploy(pool.address, ADDRESSES.AUTOMATE, ADDRESSES.WMATIC, ADDRESSES.aaveV3))
+      .not.to.be.reverted;
   });
 
   it("Should never allow reinitialization", async () => {
-    const { pool, AAVERepayPayoutAutomation } = await helpers.loadFixture(deployPoolFixture);
-    const ps = await hre.upgrades.deployProxy(AAVERepayPayoutAutomation, ["The Name", "SYMB", lp.address], {
-      kind: "uups",
-      constructorArgs: [pool.address, ADDRESSES.aaveV3],
-    });
-
-    await expect(ps.initialize("Another Name", "SYMB", lp.address)).to.be.revertedWith(
-      "Initializable: contract is already initialized"
+    const { pool, AAVERepayPayoutAutomation, maticOracle, lp } = await helpers.loadFixture(deployPoolFixture);
+    const ps = await hre.upgrades.deployProxy(
+      AAVERepayPayoutAutomation,
+      ["The Name", "SYMB", lp.address, maticOracle.address, ADDRESSES.SwapRouter, _A("0.0005")],
+      {
+        kind: "uups",
+        constructorArgs: [pool.address, ADDRESSES.AUTOMATE, ADDRESSES.WMATIC, ADDRESSES.aaveV3],
+      }
     );
+
+    await expect(
+      ps.initialize("Another Name", "SYMB", lp.address, maticOracle.address, ADDRESSES.SwapRouter, _A("0.0005"))
+    ).to.be.revertedWith("Initializable: contract is already initialized");
   });
 
   it("Should do infinite approval on initialization", async () => {
-    const { pool, AAVERepayPayoutAutomation, currency } = await helpers.loadFixture(deployPoolFixture);
-    const ps = await hre.upgrades.deployProxy(AAVERepayPayoutAutomation, ["The Name", "SYMB", lp.address], {
-      kind: "uups",
-      constructorArgs: [pool.address, ADDRESSES.aaveV3],
-    });
-
+    const { ps, currency } = await helpers.loadFixture(deployPoolAndAutomationFixture);
     expect(await currency.allowance(ps.address, ADDRESSES.aaveV3)).to.be.equal(MaxUint256);
   });
 
   it("Can create the policy through the ps and since there's no debt, deposits in AAVE", async () => {
-    const { pool, AAVERepayPayoutAutomation, rm, oracle, currency, aUSDC } = await helpers.loadFixture(
-      deployPoolFixture
-    );
+    const { pool, ps, rm, oracle, currency, aUSDC, cust } = await helpers.loadFixture(deployPoolAndAutomationFixture);
     const start = await helpers.time.latest();
-    const ps = await hre.upgrades.deployProxy(AAVERepayPayoutAutomation, ["The Name", "SYMB", lp.address], {
-      kind: "uups",
-      constructorArgs: [pool.address, ADDRESSES.aaveV3],
-    });
-
     await currency.connect(cust).approve(ps.address, _A(2000));
 
     // Create two policies, one with 1400 as price and the other with 1200
@@ -108,7 +101,7 @@ describe("Test AAVE payout automation contracts", function () {
 
     await helpers.time.increase(HOUR);
     await oracle.setPrice(_E("1390"));
-    await expect(() => rm.triggerPolicy(policyId)).to.changeTokenBalance(aUSDC, cust, _A(1000));
+    await expect(() => rm.triggerPolicy(policyId)).to.changeTokenBalance(aUSDC, cust, _A("999.992919"));
 
     await helpers.time.increase(HOUR * 24);
     const policy2 = (await rm.getPolicyData(policyId2))[0];
@@ -123,23 +116,9 @@ describe("Test AAVE payout automation contracts", function () {
   });
 
   it("Can create policies that when triggered repay stable and variable debt", async () => {
-    const {
-      pool,
-      AAVERepayPayoutAutomation,
-      rm,
-      oracle,
-      currency,
-      aave,
-      aUSDCDebtStable,
-      aUSDCDebtVariable,
-      wmatic,
-      aUSDC,
-    } = await helpers.loadFixture(deployPoolFixture);
+    const { pool, ps, rm, oracle, currency, aave, aUSDCDebtStable, aUSDCDebtVariable, wmatic, aUSDC, cust, cust2 } =
+      await helpers.loadFixture(deployPoolAndAutomationFixture);
     const start = await helpers.time.latest();
-    const ps = await hre.upgrades.deployProxy(AAVERepayPayoutAutomation, ["The Name", "SYMB", lp.address], {
-      kind: "uups",
-      constructorArgs: [pool.address, ADDRESSES.aaveV3],
-    });
 
     await currency.connect(cust).approve(ps.address, MaxUint256);
 
@@ -170,37 +149,21 @@ describe("Test AAVE payout automation contracts", function () {
 
     // Repays stable debt
     let before = await aUSDCDebtStable.balanceOf(cust.address);
-    await expect(rm.triggerPolicy(policyId))
-      .to.emit(currency, "Transfer")
-      .withArgs(ps.address, aUSDC.address, _A(1000));
-    expect(before.sub(await aUSDCDebtStable.balanceOf(cust.address))).to.be.closeTo(_A(1000), _A("0.0001"));
+    await expect(rm.triggerPolicy(policyId)).to.emit(currency, "Transfer").withArgs(ps.address, aUSDC.address, anyUint);
+    expect(before.sub(await aUSDCDebtStable.balanceOf(cust.address))).to.be.closeTo(_A(1000), _A("0.01"));
 
     // Repays variable debt
     before = await aUSDCDebtVariable.balanceOf(cust2.address);
     await expect(rm.triggerPolicy(policyId2))
       .to.emit(currency, "Transfer")
-      .withArgs(ps.address, aUSDC.address, _A(700));
-    expect(before.sub(await aUSDCDebtVariable.balanceOf(cust2.address))).to.be.closeTo(_A(700), _A("0.0001"));
+      .withArgs(ps.address, aUSDC.address, anyUint);
+    expect(before.sub(await aUSDCDebtVariable.balanceOf(cust2.address))).to.be.closeTo(_A(700), _A("0.01"));
   });
 
   it("Can create policies that when triggered repay mixed stable and variable debt", async () => {
-    const {
-      pool,
-      AAVERepayPayoutAutomation,
-      rm,
-      oracle,
-      currency,
-      aave,
-      aUSDCDebtStable,
-      aUSDCDebtVariable,
-      wmatic,
-      aUSDC,
-    } = await helpers.loadFixture(deployPoolFixture);
+    const { pool, ps, rm, oracle, currency, aave, aUSDCDebtStable, aUSDCDebtVariable, wmatic, aUSDC, cust } =
+      await helpers.loadFixture(deployPoolAndAutomationFixture);
     const start = await helpers.time.latest();
-    const ps = await hre.upgrades.deployProxy(AAVERepayPayoutAutomation, ["The Name", "SYMB", lp.address], {
-      kind: "uups",
-      constructorArgs: [pool.address, ADDRESSES.aaveV3],
-    });
 
     await currency.connect(cust).approve(ps.address, MaxUint256);
 
@@ -234,7 +197,7 @@ describe("Test AAVE payout automation contracts", function () {
 
     expect(await aUSDCDebtStable.balanceOf(cust.address)).to.be.equal(0);
     expect(await aUSDCDebtVariable.balanceOf(cust.address)).to.be.equal(0);
-    expect(await aUSDC.balanceOf(cust.address)).to.be.closeTo(_A(300), _A("0.01"));
+    expect(await aUSDC.balanceOf(cust.address)).to.be.closeTo(_A(300), _A("0.05"));
   });
 
   async function depositAndTakeDebt(aave, usdc, wmatic, debtToken, user, depositAmount, borrowAmount) {
@@ -249,30 +212,33 @@ describe("Test AAVE payout automation contracts", function () {
   async function deployPoolFixture() {
     fork(47719249);
 
-    await helpers.impersonateAccount(ADDRESSES.usrUSDC);
-    await helpers.setBalance(ADDRESSES.usrUSDC, 100n ** 18n);
-    lp = await hre.ethers.getSigner(ADDRESSES.usrUSDC);
+    const [owner, lp, cust, cust2, gelato, ...signers] = await ethers.getSigners();
 
-    const currency = await ethers.getContractAt("IERC20Metadata", ADDRESSES.usdc);
+    const currency = await initForkCurrency(
+      ADDRESSES.USDC,
+      ADDRESSES.USDCWhale,
+      [lp, cust, cust2],
+      [_A("10000"), _A("10000"), _A("10000")]
+    );
 
     const aave = await ethers.getContractAt("IPool", ADDRESSES.aaveV3);
     const aUSDC = await ethers.getContractAt("IERC20Metadata", ADDRESSES.aUSDC);
     const aUSDCDebtVariable = await ethers.getContractAt("IERC20Metadata", ADDRESSES.aUSDCDebtVariable);
     const aUSDCDebtStable = await ethers.getContractAt("IERC20Metadata", ADDRESSES.aUSDCDebtStable);
 
-    // Transfer some wmatic and USDC to the customers
-    const wmatic = await ethers.getContractAt("IERC20Metadata", ADDRESSES.wmatic);
+    // Transfer some wmatic to the customers
+    const wmatic = await ethers.getContractAt("IERC20Metadata", ADDRESSES.WMATIC);
 
-    await helpers.impersonateAccount(ADDRESSES.usrWMATIC);
-    await helpers.setBalance(ADDRESSES.usrWMATIC, 100n ** 18n);
-    const usrWMATIC = await hre.ethers.getSigner(ADDRESSES.usrWMATIC);
-    await wmatic.connect(usrWMATIC).transfer(cust.address, _E("10000"));
-    await wmatic.connect(usrWMATIC).transfer(cust2.address, _E("10000"));
+    await helpers.impersonateAccount(ADDRESSES.MATICWhale);
+    await helpers.setBalance(ADDRESSES.MATICWhale, 100n ** 18n);
+    const MATICWhale = await hre.ethers.getSigner(ADDRESSES.MATICWhale);
+    await wmatic.connect(MATICWhale).transfer(cust.address, _E("10000"));
+    await wmatic.connect(MATICWhale).transfer(cust2.address, _E("10000"));
 
     await currency.connect(lp).transfer(cust.address, _A(500));
 
     const pool = await deployPool({
-      currency: ADDRESSES.usdc,
+      currency: ADDRESSES.USDC,
       grantRoles: ["LEVEL1_ROLE", "LEVEL2_ROLE"],
       treasuryAddress: "0x87c47c9a5a2aa74ae714857d64911d9a091c25b1", // Random address
     });
@@ -295,6 +261,7 @@ describe("Test AAVE payout automation contracts", function () {
 
     const PriceOracleMock = await ethers.getContractFactory("PriceOracleMock");
     const oracle = await PriceOracleMock.deploy(_W(1500));
+    const maticOracle = await PriceOracleMock.deploy(_W("0.6"));
 
     const PriceRiskModule = await ethers.getContractFactory("PriceRiskModule");
     const rm = await addRiskModule(pool, premiumsAccount, PriceRiskModule, {
@@ -323,9 +290,36 @@ describe("Test AAVE payout automation contracts", function () {
       premiumsAccount,
       rm,
       oracle,
+      maticOracle,
       PriceRiskModule,
       PriceOracleMock,
       AAVERepayPayoutAutomation,
+      lp,
+      cust,
+      cust2,
+      gelato,
+      signers,
+    };
+  }
+
+  async function deployPoolAndAutomationFixture() {
+    const ret = await deployPoolFixture();
+    const AutomateMock = await ethers.getContractFactory("AutomateMock");
+    const automate = await AutomateMock.deploy(ret.gelato.address);
+
+    const ps = await hre.upgrades.deployProxy(
+      ret.AAVERepayPayoutAutomation,
+      ["The Name", "SYMB", ret.lp.address, ret.maticOracle.address, ADDRESSES.SwapRouter, _A("0.0005")],
+      {
+        kind: "uups",
+        constructorArgs: [ret.pool.address, automate.address, ADDRESSES.WMATIC, ADDRESSES.aaveV3],
+      }
+    );
+    return {
+      ps,
+      automate,
+      AutomateMock,
+      ...ret,
     };
   }
 });
